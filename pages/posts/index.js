141f6ba42ebea12ede82ffaf71f4a52043130a9c
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-23 18:38:50 -0500

    Refactor to add the AllPosts component
    Also needed to change the name of this page slightly
    Changed from AllPosts to AllPostsPage because of same named component
    Also has dummy data for dev purposes

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,40 @@
-export default function AllPosts() {
-  return <div>All Posts</div>;
+import AllPosts from '../../components/posts/AllPosts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting Started With Next.js',
+    image: 'getting-started-nextjs.png',
+    content:
+      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
+    date: '2022-01-15',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting Started With Next.js',
+    image: 'getting-started-nextjs.png',
+    content:
+      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
+    date: '2022-01-15',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting Started With Next.js',
+    image: 'getting-started-nextjs.png',
+    content:
+      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
+    date: '2022-01-15',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Getting Started With Next.js',
+    image: 'getting-started-nextjs.png',
+    content:
+      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
+    date: '2022-01-15',
+  },
+];
+
+export default function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
