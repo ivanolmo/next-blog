d7537038195b23941b4f14e342f1b836432b62b3
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-23 17:56:41 -0500

    Add component that will show featured posts

---
+++ components/home/FeaturedPosts.jsx
@@ -0,0 +1,12 @@
+import classes from './FeaturedPosts.module.css';
+
+function FeaturedPosts() {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+      {/* {post grid will go here} */}
+    </section>
+  );
+}
+
+export default FeaturedPosts;
