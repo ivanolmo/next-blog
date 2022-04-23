352f619a58a46d7cdeb8a2b597ff9ab835eb2652
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-23 18:22:04 -0500

    Add sub-component that renders individual posts

---
+++ components/posts/PostItem.jsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import classes from './PostItem.module.css';
+
+export default function PostItem({ post }) {
+  const { title, image, content, date, slug } = post;
+
+  const readableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  return (
+    <li className={classes.post}>
+      <Link href={`/posts/${slug}`}>
+        <a>
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${image}`}
+              alt={title}
+              layout='responsive'
+              width={300}
+              height={200}
+            />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{readableDate}</time>
+            <p>{content}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
+}
