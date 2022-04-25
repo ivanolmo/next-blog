d0f415ad719e443b63986292fcc1a7f36e15820c
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-24 22:13:44 -0500

    Refactor to replace dummy data with blog post data

--- components/posts/postDetails/PostContent.jsx
+++ components/posts/postDetails/PostContent.jsx
@@ -1,23 +1,16 @@
-import PostHeader from './PostHeader';
+import ReactMarkdown from 'react-markdown';
 
+import PostHeader from './PostHeader';
 import classes from './PostContent.module.css';
 
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs4',
-  title: 'Getting Started With Next.js',
-  image: 'getting-started-nextjs.png',
-  content:
-    '# Next.js is a React framework for production ## it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
-  date: '2022-01-15',
-};
-export default function PostContent() {
+export default function PostContent({ post }) {
   return (
     <article className={classes.content}>
       <PostHeader
-        title={DUMMY_POST.title}
-        image={`/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`}
+        title={post.title}
+        image={`/images/posts/${post.slug}/${post.image}`}
       />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
