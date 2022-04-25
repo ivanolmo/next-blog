7fdc91958eb78ecde5fc679495e642de6c77f012
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-24 21:19:03 -0500

    Replace dummy data

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,40 +1,16 @@
 import AllPosts from '../../components/posts/AllPosts';
+import { getAllPosts } from '../../lib/postUtils';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started With Next.js',
-    image: 'getting-started-nextjs.png',
-    content:
-      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
-    date: '2022-01-15',
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started With Next.js',
-    image: 'getting-started-nextjs.png',
-    content:
-      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
-    date: '2022-01-15',
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started With Next.js',
-    image: 'getting-started-nextjs.png',
-    content:
-      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
-    date: '2022-01-15',
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started With Next.js',
-    image: 'getting-started-nextjs.png',
-    content:
-      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
-    date: '2022-01-15',
-  },
-];
+export default function AllPostsPage({ allPosts }) {
+  return <AllPosts posts={allPosts} />;
+}
 
-export default function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      allPosts,
+    },
+  };
 }
