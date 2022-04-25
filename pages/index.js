220c7f9f896b274136f0c1e3106f03e0308abf9b
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-24 21:18:38 -0500

    Remove dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,22 @@
 import FeaturedPosts from '../components/home/FeaturedPosts';
 import Hero from '../components/home/Hero';
+import { getFeaturedPosts } from '../lib/postUtils';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started With Next.js',
-    image: 'getting-started-nextjs.png',
-    content:
-      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
-    date: '2022-01-15',
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started With Next.js',
-    image: 'getting-started-nextjs.png',
-    content:
-      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
-    date: '2022-01-15',
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started With Next.js',
-    image: 'getting-started-nextjs.png',
-    content:
-      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
-    date: '2022-01-15',
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started With Next.js',
-    image: 'getting-started-nextjs.png',
-    content:
-      'Next.js is a React framework for production - it makes building full stack React applications a breeze, and comes with built-in features like Server Side Rendering.',
-    date: '2022-01-15',
-  },
-];
-
-export default function Home() {
+export default function Home({ featuredPosts }) {
   return (
     <div>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS} />
+      <FeaturedPosts posts={featuredPosts} />
     </div>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      featuredPosts,
+    },
+  };
 }
