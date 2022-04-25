c222c522c3c994aba23ebbd5ea0c8ec86197e983
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-24 22:13:17 -0500

    Refactor with SSG functionality

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,5 +1,26 @@
 import PostContent from '../../components/posts/postDetails/PostContent';
+import { getPostData, getAllPosts } from '../../lib/postUtils';
 
-export default function Post() {
-  return <PostContent />;
+export default function Post({ post }) {
+  return <PostContent post={post} />;
+}
+
+export function getStaticProps(context) {
+  const slug = context.params.slug;
+  const post = getPostData(slug);
+
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export function getStaticPaths() {
+  const posts = getAllPosts();
+
+  return {
+    paths: posts.map((post) => ({ params: { slug: post.slug } })),
+    fallback: false,
+  };
 }
