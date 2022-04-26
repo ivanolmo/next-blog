ad0b744f257eb57b15ed4466614096e4e52699a8
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-26 00:12:45 -0500

    Refactor to have functioning form submission

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -1,23 +1,47 @@
+import { useRef } from 'react';
+
 import classes from './ContactForm.module.css';
 
 export default function ContactForm() {
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const messageRef = useRef();
+
+  function contactHandler(e) {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    const name = nameRef.current.value;
+    const message = messageRef.current.value;
+
+    fetch('/api/contactForm', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, name, message }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <section className={classes.contact}>
       <h1>Contact me today!</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={contactHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor='email'>Your Email</label>
-            <input type='email' id='email' required />
+            <input type='email' id='email' ref={emailRef} required />
           </div>
           <div className={classes.control}>
             <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' required />
+            <input type='text' id='name' ref={nameRef} required />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor='message'>Your Message</label>
-          <textarea id='message' rows='5'></textarea>
+          <textarea id='message' rows='5' ref={messageRef}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
